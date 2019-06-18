4a8e5be1174675442a142c87f3ca08a9dc6efdd4
WouterDePrins <wouterdeprins@hotmail.com>
2019-06-18 14:22:19 +0200

    Update ubuntuDockerInstall.sh

--- ubuntuDockerInstall.sh
+++ ubuntuDockerInstall.sh
@@ -1,2 +1,7 @@
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+#### Install Docker on Ubuntu
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
+
+source /usr/local/cliqr/etc/userenv
+export IPAddr
+ifconfig ens160 $IPAddr netmask 255.255.255.0
