950557b4ac6745d9c93eaf5d04c62027f845162d
WouterDePrins <wouterdeprins@hotmail.com>
2019-06-25 13:54:36 +0200

    Update ubuntuDockerInstall.sh

--- ubuntuDockerInstall.sh
+++ ubuntuDockerInstall.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#### Install Docker on Ubuntu
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh get-docker.sh
-
-
-#### Set Static IP 
-#source /usr/local/cliqr/etc/userenv
-#export IPAddr
-#ifconfig ens160 $IPAddr netmask 255.255.255.0
-
-# Install django
 sudo apt-get update -y
 sudo apt install python-pip -y
 sudo apt-get install git -y
