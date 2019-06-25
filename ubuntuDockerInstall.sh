55698e0d64e376095c043273a72da2d225529d0d
WouterDePrins <wouterdeprins@hotmail.com>
2019-06-25 14:19:32 +0200

    Update ubuntuDockerInstall.sh

--- ubuntuDockerInstall.sh
+++ ubuntuDockerInstall.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sudo apt-get update -y
+sudo apt install python-pip -y
+sudo apt-get install git -y
+sudo apt-get install git -y
+sudo pip3 install Django
+
+cd /home
+mkdir Coding
+cd Coding
+
+sudo git config --global user.email “wouterdeprins@hotmail.com”
+sudo git config --global user.name “WouterDePrins”
+sudo git clone https://github.com/WouterDePrins/EasyRefApp.git
