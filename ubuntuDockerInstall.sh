9039862bb3dae38827e3c5594936e74e5584dd65
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-26 17:40:58 +0100

    Update ubuntuDockerInstall.sh

--- ubuntuDockerInstall.sh
+++ ubuntuDockerInstall.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-sudo apt-get install python3 python3-pip git -y
-sudo pip3 install Django
-
-cd /home
-mkdir Coding
-cd Coding
-
-sudo git config --global user.email "wouterdeprins@hotmail.com"
-sudo git config --global user.name "WouterDePrins"
-sudo git clone https://github.com/WouterDePrins/CCS_IPAM.git
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
