c4338790c379e5b97f0ca4bcf8249148092a643e
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-27 09:37:42 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,4 +1,29 @@
-sudo mkdir -p /home/cliqruser/db/
-cd /home/cliqruser/db
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y install aptitude
+sudo apt-get -y install mysql-server
+sudo apt-get -y install git
+sudo aptitude -y install expect
+MYSQL_ROOT_PASSWORD=ciscocisco
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$MYSQL\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+sudo echo "$SECURE_MYSQL"
+sudo aptitude -y purge expect
+cd /home/cliqruser/
 sudo git clone https://github.com/datacharmer/test_db.git
-sudo mysql -u root --password="ciscocisco" --connect-expired-password < /home/cliqruser/db/employees.sql
+sudo mysql -u root --password="ciscocisco" --connect-expired-password < /home/cliqruser/test_db/employees.sql
