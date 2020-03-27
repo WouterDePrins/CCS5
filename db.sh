b436239daba4751cd2e408976a01a997aa75c32e
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-27 17:10:38 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -6,7 +6,7 @@ sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('something');FLUSH PRI
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 sudo mysql -e "DROP DATABASE test;DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';"
-sudo mysql -u root -psomething -e "CREATE USER 'ubuntu'@'localhost' IDENTIFIED BY 'something';GRANT ALL PRIVILEGES ON *.* TO 'ubuntu'@'localhost';FLUSH PRIVILEGES;"
+sudo mysql -u root -psomething -e "CREATE USER 'ubuntu'@'localhost' IDENTIFIED BY 'something';GRANT ALL PRIVILEGES ON *.* TO 'ubuntu'@'%';FLUSH PRIVILEGES;"
 cd /home/cliqruser/
 sudo git clone https://github.com/datacharmer/test_db.git
 cd /home/cliqruser/test_db/
