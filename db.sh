530ccd2b252e9c31a5da1473a1a8374fb691cc12
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-27 09:29:26 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,4 +1,4 @@
 sudo mkdir -p /home/cliqruser/db/
-sudo cd /home/cliqruser/db
+cd /home/cliqruser/db
 sudo git clone https://github.com/datacharmer/test_db.git
 sudo mysql -u root --password="ciscocisco" --connect-expired-password < /home/cliqruser/db/employees.sql
