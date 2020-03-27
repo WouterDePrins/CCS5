2eec2a1610a4f3d6ede29ecbebdbcaf14d5b7ff2
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-27 15:21:30 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y install build-essential mysql-server libmysqlclient-dev npm git mysql-server
+sudo apt-get -y install mysql-server 
+sudo apt-get -y install git
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('something');FLUSH PRIVILEGES;"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
