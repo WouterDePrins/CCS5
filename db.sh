5fbf9c79234be73c9afa7c7b51924fc088422cfa
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-27 17:59:31 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -2,12 +2,13 @@
 sudo apt-get -y update
 sudo apt-get -y install mysql-server 
 sudo apt-get -y install git
-sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i "s/.*bind-address.*/#bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('something');FLUSH PRIVILEGES;"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 sudo mysql -e "DROP DATABASE test;DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';"
-sudo mysql -u root -psomething -e "CREATE USER 'ubuntu'@'localhost' IDENTIFIED BY 'something';GRANT ALL PRIVILEGES ON *.* TO 'ubuntu'@'%';FLUSH PRIVILEGES;"
+sudo mysql -u root -psomething -e "CREATE USER 'ubuntu'@'localhost' IDENTIFIED BY 'something';"
+sudo mysql -u root -psomething -e "GRANT ALL PRIVILEGES ON *.* TO ubuntu@'%' IDENTIFIED BY 'something' WITH GRANT OPTION;"
 cd /home/cliqruser/
 sudo git clone https://github.com/datacharmer/test_db.git
 cd /home/cliqruser/test_db/
