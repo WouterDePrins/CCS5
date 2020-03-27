4f039b437b128b14444c7f9f7e78fe98e8568515
WouterDePrins <wouterdeprins@hotmail.com>
2020-03-27 09:58:28 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -26,4 +26,6 @@ sudo echo "$SECURE_MYSQL"
 sudo aptitude -y purge expect
 cd /home/cliqruser/
 sudo git clone https://github.com/datacharmer/test_db.git
-sudo mysql -u root --password="ciscocisco" --connect-expired-password < test_db/employees.sql
+sleep 3s
+cd /test_db
+sudo mysql -u root --password="ciscocisco" --connect-expired-password < employees.sql
